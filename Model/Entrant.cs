f2e527adae31af691203199125cc4248b4a5070d
Dmitry Vasyliev <dv4code@gmail.com>
2021-05-21 13:45:43 +0300

    Basic model done.

---
+++ Model/Entrant.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Model
+{
+    public struct Grades
+    {
+        public Grades(int exam1, int exam2, int exam3, int certificate)
+        {
+            Exam1 = exam1;
+            Exam2 = exam2;
+            Exam3 = exam3;
+            Certificate = certificate;
+        }
+
+        public int Exam1;
+        public int Exam2;
+        public int Exam3;
+        public int Certificate;
+    }
+
+    public struct EntrantInfo
+    {
+        public EntrantInfo(string name, string middleName, string surname, DateTime birthDate)
+        {
+            Name = name;
+            MiddleName = middleName;
+            Surname = surname;
+            BirthDate = birthDate;
+        }
+
+        public string Name;
+        public string MiddleName;
+        public string Surname;
+        public DateTime BirthDate;
+    }
+    
+    public class Entrant
+    {
+        public EntrantInfo Info { get; }
+        public Grades Grades { get; }
+        public bool AgreesToContract { get; }
+
+        public Entrant(EntrantInfo info, Grades grades, bool agreesToContract)
+        {
+            Info = info;
+            Grades = grades;
+            AgreesToContract = agreesToContract;
+        }
+    }
+}
